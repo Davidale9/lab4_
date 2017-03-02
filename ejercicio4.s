f8e62f35f17e601cb4459b39f2c07d5cbb038e76
Davidale9 <16004593@galileo.edu>
2017-03-01 18:16:22 -0800

    asdasd2

---
+++ ejercicio4.s
@@ -0,0 +1,38 @@
+.data
+	String: .asciz " %d \n"
+.text
+  .globl main
+
+
+main:
+    ldr x21, [x1, 8]
+    mov x0, x21
+    bl atoi
+fact:
+    add SP, SP, #-16
+    str x19, [sp, 0]
+    str x30, [SP, 8]         // guardamos x30 para poder llamar a funciones
+
+    mov x19 x0
+    cmp x19, 1
+    b.lt cambio
+    sub x0, x19, 1
+    bl. fact
+    mul  x0, x19, x0
+    b fin-fact
+cambio:
+    mov x0, 1
+    b. fin-fact
+fin-fact:
+    mov x1, x0
+    ldr x0, =String
+    bl printf
+    b exit
+
+exit:
+    // restauramos los registros
+    mov x1, x30
+    ldr x19, [sp, 0]
+    ldr x30, [SP, 8]
+    add SP, SP, #16 // retornamos el espacio prestado del stack
+    ret // retornamos al SO
